06c2497d15e9569c28881b906021e0e8c93c323a
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-17 15:34:22 +0300

    Added Typescript property modifiers for Teacher's interface

--- task_1/main.ts
+++ task_1/main.ts
@@ -1,8 +1,8 @@
 interface Teacher {
-  firstName: string;
-  lastName: string;
+  readonly firstName: string;
+  readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  contract: boolean;
+  [key: string]: any;
 }
