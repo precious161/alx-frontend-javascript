3cb20a199cf4288b3a4605cb6adf7b5a9a1867f0
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-18 10:06:00 +0300

    Added Directors interface

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -6,3 +6,13 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+  numberOfReports: number;
+}
