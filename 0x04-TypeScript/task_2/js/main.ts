1a65b60d16bcda6b411e7a2956725c717d6a3e98
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-19 12:18:46 +0300

    Practices string literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,12 @@ function executeWork(employee: Director | Teacher): string {
     return employee.workTeacherTasks();
   }
 }
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if ((todayClass = "History")) {
+    return "Teaching History";
+  }
+}
